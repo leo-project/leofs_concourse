1bce4179c81caad67de9ff43ad5d1c6ed5fd0d68
Wilson Li <windkithk@gmail.com>
2016-06-21 14:44:13 +0900

    [TMP] Ansible Pipeline

---
+++ files/deploy_leofs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+cd leofs_ansible
+
+echo $1 > ansible_key
+sed -i -e 's/ /\n/g; s/BEGIN\nRSA\nPRIVATE\nKEY/BEGIN RSA PRIVATE KEY/g; s/END\nRSA\nPRIVATE\nKEY/END RSA PRIVATE KEY/g' ansible_key
+chmod 600 ansible_key
+
+cp ../leofs_concourse/files/deploy_leofs.yml .
+
+ansible-playbook -i ../leofs_concourse/files/hosts.small purge_leofs.yml -u wilson --private-key=ansible_key 
+ansible-playbook -i ../leofs_concourse/files/hosts.small build_leofs.yml -u wilson --private-key=ansible_key 
+ansible-playbook -i ../leofs_concourse/files/hosts.small deploy_leofs.yml -u wilson --private-key=ansible_key 
+
+./leofs-adm status
+./leofs-adm add-endpoint 192.168.100.35
+sleep 5
+./leofs-adm add-endpoint 192.168.100.35
