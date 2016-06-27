640a0c8b2bca7d2e605e587dce27c6201c011213
Wilson Li <windkithk@gmail.com>
2016-06-27 11:00:20 +0900

    Break down client tests

--- TestScenario1/files/run_client_tests.sh
+++ TestScenario1/files/run_client_tests.sh
@@ -2,25 +2,41 @@
 set -x
 set -e
 
-host=$1
-port=$2
+sdk=$1
+host=$2
+port=$3
 
 cd leofs_client_tests
 (cd temp_data; ./gen.sh)
+
 ## aws-sdk-go only v4
 #(export GOPATH=$HOME/go; cd aws-sdk-go; go run LeoFSTest.go v4 $host $port testg)
 
-(cd aws-sdk-java; ant -Dsignver=v2 -Dhost=$host -Dport=$port -Dbucket="testj")
+cd $sdk
+if [ $sdk == "aws-sdk-java" ]
+then
+	(ant -Dsignver=v2 -Dhost=$host -Dport=$port -Dbucket="testj")
 
-(echo "$host testp.$host" >> /etc/hosts)
-(cd aws-sdk-php; curl -sS https://getcomposer.org/installer | php; php composer.phar install; php LeoFSTest.php v2 $host $port testp)
+elif [ $sdk == "aws-sdk-php" ]
+then
+	(echo "$host testp.$host" >> /etc/hosts)
+	(curl -sS https://getcomposer.org/installer | php; php composer.phar install; php LeoFSTest.php v2 $host $port testp)
 
-(echo "$host testr.$host" >> /etc/hosts)
-(cd aws-sdk-ruby; ruby LeoFSTest.rb v2 $host $port testr)
+elif [ $sdk == "aws-sdk-ruby" ]
+then
+	(echo "$host testr.$host" >> /etc/hosts)
+	(ruby LeoFSTest.rb v2 $host $port testr)
 
-(cd boto; python LeoFSTest.py v2 $host $port testb)
+elif [ $sdk == "boto" ]
+then
+	(python LeoFSTest.py v2 $host $port testb)
 
-(echo "$host teste.$host" >> /etc/hosts)
-(cd erlcloud; make deps; make compile; ./LeoFSTest.erl v2 $host $port teste)
+elif [ $sdk == "erlcloud" ]
+then
+	(echo "$host teste.$host" >> /etc/hosts)
+	(make deps; make compile; ./LeoFSTest.erl v2 $host $port teste)
 
-(cd jclouds; mvn dependency:copy-dependencies; ant -Dsignver=v2 -Dhost=$host -Dport=$port -Dbucket="testj")
+elif [ $sdk == "jclouds" ]
+then
+	(mvn dependency:copy-dependencies; ant -Dsignver=v2 -Dhost=$host -Dport=$port -Dbucket="testj")
+fi
