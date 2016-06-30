a3e68d2c4784dd69f52391b00c397adecc4aa796
Wilson Li <windkithk@gmail.com>
2016-06-30 14:06:15 +0900

    [S4] Added Medium Cluster Inventory

---
+++ S4/files/deploy_leofs_medium.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+cd leofs_ansible
+
+echo $1 > ansible_key
+sed -i -e 's/ /\n/g; s/BEGIN\nRSA\nPRIVATE\nKEY/BEGIN RSA PRIVATE KEY/g; s/END\nRSA\nPRIVATE\nKEY/END RSA PRIVATE KEY/g' ansible_key
+chmod 600 ansible_key
+
+cp ../leofs_concourse/S4/files/deploy_leofs.yml .
+
+ansible-playbook -i ../leofs_concourse/S4/files/hosts.medium purge_leofs.yml -u wilson --private-key=ansible_key 
+ansible-playbook -i ../leofs_concourse/S4/files/hosts.medium build_leofs.yml -u wilson --private-key=ansible_key 
+ansible-playbook -i ../leofs_concourse/S4/files/hosts.medium deploy_leofs.yml -u wilson --private-key=ansible_key 
+
+./leofs-adm status
+./leofs-adm add-endpoint 192.168.100.35
+sleep 5
+./leofs-adm add-endpoint 192.168.100.35
+
+./leofs-adm add-bucket test 05236
