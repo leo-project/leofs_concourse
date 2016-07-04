85580bd5e6eb3a63da69fb257ad10390e68f4cbe
Wilson Li <windkithk@gmail.com>
2016-07-04 10:31:20 +0900

    Restructure Files

--- S4/files/build_basho_bench.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd leofs_basho_bench_src;
-make;
-cd ..
-cp -r leofs_basho_bench_src/* leofs_basho_bench_bin
--- scripts/set_basho_bench_conf.sh
+++ scripts/set_basho_bench_conf.sh
@@ -2,7 +2,7 @@
 
 LEOFS_GW_HOST=$1
 LEOFS_GW_PORT=$2
-TEMPLATE_PATH="leofs_concourse/S4/files/templates"
+TEMPLATE_PATH="leofs_concourse/templates"
 CONF_PATH="leofs_basho_bench_conf"
 
 shopt -s nullglob
