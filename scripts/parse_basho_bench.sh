826486af5d0a3468e341d106e6879c92becd1f8a
Wilson Li <windkithk@gmail.com>
2016-07-05 10:39:36 +0900

    [WIP] Basho Bench Results

---
+++ scripts/parse_basho_bench.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+set -x
+
+TEST_NAME=$BASHO_BENCH_TEST_NAME
+ERROR_THRES=$BASHO_BENCH_ERROR_THRES
+GRAFANA_BASE="http://192.168.100.31:3000"
+LEOFS_MANAGER_HOST="192.168.100.35"
+LEOFS_MANAGER_PORT=10010
+
+cd leofs_basho_bench_result/result
+
+ERROR=$(tail -n +2 summary.csv | awk '{total+=$3; err+=$5}END{print err/total}')
+FAILED=$(awk -v err=$ERROR -v thres=$ERROR_THRES 'BEGIN{if (err >= thres) {print 1} else {print 0}}')
+
+echo "Error Rate: $ERROR"
+
+if [ $FAILED -eq 1 ]; then
+	echo "Exceeded Threshold $ERROR_THRES"
+	exit 1
+fi
+
+echo "status" | nc $LEOFS_MANAGER_HOST $LEOFS_MANAGER_PORT > status_file
+
+MONITOR_PERIOD=$(cat monitor_period)
+
+TZ=Asia/Tokyo /phantomjs-2.1.1-linux-x86_64/bin/phantomjs /phantomjs-2.1.1-linux-x86_64/bin/capture_5s.js $GRAFANA_BASE/dashboard/db/leofs-dashboard-ci$MONITOR_PERIOD grafana.png "1920px"
+
+cd ../../
+git clone leofs_notes leofs_notes_update
+cd leofs_notes_update
+
+TIME=$(TZ=Asia/Tokyo date +"%Y%m%d_%H%M")
+mkdir ${TIME}_$TEST_NAME
+(cd ${TIME}_$TEST_NAME && cp -r ../../leofs_basho_bench_result/result/* .)
+
+git config --global user.email "nobody@concourse.ci"
+git config --global user.name "Concourse"
+
+git add .
+git commit -m "Added Test Case $TEST_NAME [$TIME]"
--- scripts/run_basho_bench.sh
+++ scripts/run_basho_bench.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+LEAD_IN=30000
+LEAD_OUT=60000
+
 TEST_NAME=$BASHO_BENCH_TEST_NAME
 LOADER_CONF=$BASHO_BENCH_LOAD_CONF
 TEST_CONF=$BASHO_BENCH_TEST_CONF
@@ -11,6 +14,17 @@ cd leofs_basho_bench_bin
 tail ${TEST_NAME}.load.log
 echo "Cooldown, Sleep 60 seconds"
 sleep 60
+
+echo "Start"
+START_EPOCH=$(($(TZ=Asia/Tokyo date +%s%N)/1000000-$LEAD_IN))
 ./basho_bench ../leofs_basho_bench_conf/$TEST_CONF > ${TEST_NAME}.log
+END_EPOCH=$(($(TZ=Asia/Tokyo date +%s%N)/1000000+$LEAD_OUT))
 tail ${TEST_NAME}.log
+
+echo "Cooldown, Sleep $(($LEAD_OUT/1000))"
+sleep $(($LEAD_OUT/1000))
+
 make results
+cp -rL tests/current ../leofs_basho_bench_result/result
+echo "?from=${START_EPOCH}&to=${END_EPOCH}" > ../leofs_basho_bench_result/result/monitor_period
+cp *.log ../leofs_basho_bench_result/result
